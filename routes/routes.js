820779c46b492418ce873b9a6b6dbff40b923b08
James Snell <Vulpes3.1415@yahoo.com>
2019-11-01 19:02:00 -0600

    Heroku deploy

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@
 var axios = require("axios");
 var cheerio = require("cheerio");
 var latestNews = require("../models/website.js");
+
 module.exports = function (app) {
     //routes go here
     app.get("/", function(req, res) {
